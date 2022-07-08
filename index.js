98cf9fe4e7040f8f1129aeeadf4dbd92163e6222
い 狂奔的蜗牛 <137224350@qq.com>
2022-07-08 23:26:48 +0800

    feat: 新增图片压缩功能与优化代码

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 #!/usr/bin/env node
-const program = require('commander');
-const init = require('./lib/core/Init');
-const helpOptions = require('./lib/core/Help');
-const createCommands = require('./lib/core/Create');
-//  从package.json中读取版本信息
-program.version(require('./package.json').version);
-init().then(() => {
-    // 设置可选项
-    helpOptions();
+const importLocal = require('import-local');
+const log = require('./lib/utils/Log');
 
-    // 创建其他指令
-    createCommands();
-
-    // 解析参数
-    program.parse(process.argv);
-});
+if (importLocal(__filename)) { // 使用本地的版本
+    log.info('', '正在使用 mini-core-cli 本地版本');
+} else {
+    // 加载lib下index并传入cli后的参数
+    require('./lib/core/Init')(process.argv.slice(2));
+}
