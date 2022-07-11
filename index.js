559df81ba6fcf49d193cfda2e9e9dd376f38a06e
い 狂奔的蜗牛 <137224350@qq.com>
2022-07-11 23:00:03 +0800

    fix: 修复因获取当前路径在mac上和windows上不同而导致的bug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 const importLocal = require('import-local');
 const log = require('./lib/utils/Log');
-
-if (importLocal(__filename)) { // 使用本地的版本
+if (importLocal(__filename)) {
+    // 使用本地的版本
     log.info('', '正在使用 mini-core-cli 本地版本');
 } else {
     // 加载lib下index并传入cli后的参数
